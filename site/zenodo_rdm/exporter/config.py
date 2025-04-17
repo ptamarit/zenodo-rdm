db3636140bd7ad3a5592e51d2e3c1250f8a82f02
Pablo Tamarit <pablo.tamarit@cern.ch>
2025-04-17 14:38:50 +0200

    exporter: module and blueprint

---
+++ site/zenodo_rdm/exporter/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 CERN.
+#
+# ZenodoRDM is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""ZenodoRDM exporter configuration."""
+
+from uuid import UUID
+
+# Similar to legacy exporter: https://github.com/zenodo/zenodo/blob/master/zenodo/modules/exporter/config.py
+EXPORTER_BUCKET_UUID = UUID("00000000-0000-0000-0000-000000000001")
